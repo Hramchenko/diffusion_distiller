d29e59bf081a39028eedbaec78831b402250b036
Vitaliy Hramchenko <hramchenko@bk.ru>
2022-01-20 11:04:53 +0500

    Rosanality DDPM sampler was used.

--- v_diffusion.py
+++ v_diffusion.py
@@ -40,7 +40,8 @@ def noise_like(shape, noise_fn, device, repeat=False):
 
 
 class GaussianDiffusion:
-    def __init__(self, net, betas, time_scale=1):
+
+    def __init__(self, net, betas, time_scale=1, sampler="ddpm"):
         super().__init__()
         self.net_ = net
         self.time_scale = time_scale
@@ -53,10 +54,20 @@ class GaussianDiffusion:
             (torch.tensor([1], dtype=torch.float64, device=betas.device), alphas_cumprod[:-1]), 0
         )
         posterior_variance = betas * (1 - alphas_cumprod_prev) / (1 - alphas_cumprod)
+
         self.betas = betas
         self.alphas_cumprod = alphas_cumprod
+        self.posterior_variance = posterior_variance
         self.sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
         self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1 - alphas_cumprod)
+        self.posterior_log_variance_clipped = torch.log(posterior_variance.clamp(min=1e-20))
+        self.posterior_mean_coef1 = (betas * torch.sqrt(alphas_cumprod_prev) / (1 - alphas_cumprod))
+        self.posterior_mean_coef2 = (1 - alphas_cumprod_prev) * torch.sqrt(alphas) / (1 - alphas_cumprod)
+
+        if sampler == "ddpm":
+            self.p_sample = self.p_sample_ddpm
+        else:
+            self.p_sample = self.p_sample_clipped
 
     def inference(self, x, t, extra_args):
         return self.net_(x, t*self.time_scale, **extra_args)
@@ -70,7 +81,30 @@ class GaussianDiffusion:
         v = alpha_t*noise-sigma_t*x_0
         return F.mse_loss(v_recon, v.float())
 
-    def p_sample(self, x, t, extra_args, eta=0, clip_denoised=True, clip_value=3):
+    def q_posterior(self, x_0, x_t, t):
+        mean = E_(self.posterior_mean_coef1, t, x_t.shape) * x_0 \
+               + E_(self.posterior_mean_coef2, t, x_t.shape) * x_t
+        var = E_(self.posterior_variance, t, x_t.shape)
+        log_var_clipped = E_(self.posterior_log_variance_clipped, t, x_t.shape)
+        return mean, var, log_var_clipped
+
+    def p_mean_variance(self, x, t, extra_args, clip_denoised):
+        v = self.inference(x.float(), t.float(), extra_args).double()
+        alpha_t, sigma_t = self.get_alpha_sigma(x, t)
+        x_recon = alpha_t*x - sigma_t*v
+        if clip_denoised:
+            x_recon = x_recon.clamp(min=-1, max=1)
+        mean, var, log_var = self.q_posterior(x_recon, x, t)
+        return mean, var, log_var
+
+    def p_sample_ddpm(self, x, t, extra_args, clip_denoised=True, **kwargs):
+        mean, _, log_var = self.p_mean_variance(x, t, extra_args, clip_denoised)
+        noise = torch.randn_like(x)
+        shape = [x.shape[0]] + [1] * (x.ndim - 1)
+        nonzero_mask = (1 - (t == 0).type(torch.float32)).view(*shape)
+        return mean + nonzero_mask * torch.exp(0.5 * log_var) * noise
+
+    def p_sample_clipped(self, x, t, extra_args, eta=0, clip_denoised=True, clip_value=3):
         v = self.inference(x.float(), t, extra_args)
         alpha, sigma = self.get_alpha_sigma(x, t)
         # if clip_denoised:
@@ -82,29 +116,6 @@ class GaussianDiffusion:
         if clip_denoised:
             eps = eps.clip(-clip_value, clip_value)
 
-        t_mask = (t > 0)
-        if t_mask.any().item():
-            if not t_mask.all().item():
-                raise Exception()
-            alpha_, sigma_ = self.get_alpha_sigma(x, (t - 1).clip(min=0))
-            ddim_sigma = eta * (sigma_ ** 2 / sigma ** 2).sqrt() * \
-                         (1 - alpha ** 2 / alpha_ ** 2).sqrt()
-            adjusted_sigma = (sigma_ ** 2 - ddim_sigma ** 2).sqrt()
-            pred = pred * alpha_ + eps * adjusted_sigma
-            if eta:
-                pred += torch.randn_like(pred) * ddim_sigma
-        return pred
-
-    def p_sample_master(self, x, t, extra_args, eta=0, clip_denoised=True, clip_value=3):
-        v = self.inference(x.float(), t, extra_args)
-        alpha, sigma = self.get_alpha_sigma(x, t)
-        pred = (x * alpha - v * sigma)
-        if clip_denoised:
-            pred = pred.clip(-clip_value, clip_value)
-        eps = (x - alpha*pred)/sigma
-
-        if clip_denoised:
-            eps = eps.clip(-clip_value, clip_value)
         t_mask = (t > 0)
         if t_mask.any().item():
             if not t_mask.all().item():
